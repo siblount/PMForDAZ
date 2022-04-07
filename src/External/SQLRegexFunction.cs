855e9a5de6a50c1a97079a5339ffbd0c404a7653
Solomon <solomon1blount@gmail.com>
2022-04-07 09:26:54 -0400

    Create SQLRegexFunction.cs
    StackOverflow code in preparations for installing REGEXP function for the SQLite Database.

---
+++ src/External/SQLRegexFunction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAZ_Installer.External
+{
+    // Found from https://stackoverflow.com/questions/24229785/sqlite-net-sqlitefunction-not-working-in-linq-to-sql/26155359#26155359
+    // taken from http://sqlite.phxsoftware.com/forums/p/348/1457.aspx#1457
+    [SQLiteFunction(Name = "REGEXP", Arguments = 2, FuncType = FunctionType.Scalar)]
+    internal class SQLRegexFunction : SQLiteFunction
+    {
+        public override object Invoke(object[] args)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(Convert.ToString(args[1]), Convert.ToString(args[0]));
+        }
+
+        public void BindFunction(SQLiteConnection connection)
+        {
+            var attributes = GetType().GetCustomAttributes(typeof(SQLiteFunctionAttribute), true).Cast<SQLiteFunctionAttribute>().ToArray();
+            if (attributes.Length == 0)
+            {
+                throw new InvalidOperationException("SQLiteFunction doesn't have SQLiteFunctionAttribute");
+            }
+            connection.BindFunction(attributes[0], this);
+        }
+    }
+}
