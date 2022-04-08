d2713c9979499e57ed710aed987c7be64b3fc57f
Solomon <solomon1blount@gmail.com>
2022-04-08 16:06:24 -0400

    Seperated record class + added new DPSearchRecord.

--- src/DP/DPExtractionRecord.cs
+++ src/DP/DPExtractionRecord.cs
@@ -1,11 +1,6 @@
 ﻿// This code is licensed under the Keep It Free License V1.
 // You may find a full copy of this license at root project directory\LICENSE
-
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAZ_Installer
 {
@@ -16,12 +11,4 @@ namespace DAZ_Installer
     {
 
     }
-
-    // TODO: Update product record.
-    internal record DPProductRecord(string ProductName, string[] Tags, string[] Directories, DateTime Time, string[] FilesExtracted, string ExpectedExtractionRecordLocation, string ExpectedImageLocation, uint ID)
-    {
-        
-    }
-
-    // TODO: Create simplified search record.
 }
---
+++ src/DP/DPProductRecord.cs
@@ -0,0 +1,11 @@
+﻿// This code is licensed under the Keep It Free License V1.
+// You may find a full copy of this license at root project directory\LICENSE
+using System;
+
+namespace DAZ_Installer
+{
+    internal record DPProductRecord(string ProductName, string[] Tags, string[] Directories, DateTime Time, string[] FilesExtracted, string ExpectedExtractionRecordLocation, string ExpectedImageLocation, uint ID)
+    {
+
+    }
+}
