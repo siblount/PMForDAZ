82b099d1820b879c745d1af02029b92ac5f44d05
Solomon <solomon1blount@gmail.com>
2022-07-01 12:49:47 -0400

    contain is now an alias of indexof

--- src/Utilities/SpanExtensions.cs
+++ src/Utilities/SpanExtensions.cs
@@ -12,36 +12,8 @@ namespace DAZ_Installer.Utilities
         /// <param name="msg">The msg to find inside span.</param>
         /// <returns><see langword="true"/> if <paramref name="span"/> contains <paramref name="msg"/>.
         /// Otherwise, <see langword="false"/>.</returns>
-        public static bool Contains(this Span<char> span, string msg)
-        {
-            if (msg.Length > span.Length) return false;
-            for (int i = 0; i < span.Length; i++)
-            {
-                var contains = false;
-                for (int j = 0; j < msg.Length; j++)
-                {
-                    if (i + j >= span.Length || span[i + j] != msg[j]) break;
-                    contains = true;
-                }
-                if (contains) return true;
-            }
-            return false;
-        }
+        public static bool Contains(this Span<char> span, string msg) => span.IndexOf(msg) != -1;
         /// <inheritdoc cref="Contains(Span{char}, string)"/>
-        public static bool Contains(this ReadOnlySpan<char> span, string msg)
-        {
-            if (msg.Length > span.Length) return false;
-            for (int i = 0; i < span.Length; i++)
-            {
-                var contains = false;
-                for (int j = 0; j < msg.Length; j++)
-                {
-                    if (i + j >= span.Length || span[i + j] != msg[j]) break;
-                    contains = j == msg.Length - 1;
-                }
-                if (contains) return true;
-            }
-            return false;
-        }
+        public static bool Contains(this ReadOnlySpan<char> span, string msg) => span.IndexOf(msg) != -1;
     }
 }
