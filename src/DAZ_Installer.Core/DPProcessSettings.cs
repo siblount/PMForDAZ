79504c567bf77ef4bf1f1530775efc14a2ba6e4f
Solomon Blount <74916907+siblount@users.noreply.github.com>
2023-01-05 19:46:18 -0500

    Added new DPProcessSettings

---
+++ src/DAZ_Installer.Core/DPProcessSettings.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace DAZ_Installer.Core
+{
+    public struct DPProcessSettings
+    {
+        string TempPath;
+        string DestinationPath;
+        RecycleOption DeleteAction;
+        string[] ContentFolders;
+        Dictionary<string, string> ContentRedirectFolders;
+    }
+}
