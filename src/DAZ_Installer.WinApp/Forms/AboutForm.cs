957efded61e9f86708739c1abbe810c9082b7a82
Solomon Blount <74916907+siblount@users.noreply.github.com>
2023-11-14 20:03:49 -0500

    Remove duplicate WinApp project.

--- src/DAZ_Installer.WinApp/Forms/AboutForm.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Forms;
-
-namespace DAZ_Installer.WinApp.Forms
-{
-    public partial class AboutForm : Form
-    {
-        public AboutForm() => InitializeComponent();
-    }
-}
