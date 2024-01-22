106f1ad4cc575d352ce70aadfd0394ea6e333dfc
Solomon Blount <74916907+siblount@users.noreply.github.com>
2024-01-22 15:44:36 -0500

    doc update for DPProductRecordLite

--- src/DAZ_Installer.Database/DPProductRecordLite.cs
+++ src/DAZ_Installer.Database/DPProductRecordLite.cs
@@ -6,6 +6,9 @@ namespace DAZ_Installer.Database
     /// <summary>
     /// A lite version of <see cref="DPProductRecord"/>. This is used for the search results.
     /// </summary>
-
-    public record struct DPProductRecordLite(string Name, string Thumbnail, IReadOnlyList<string> Tags, long ID) { }
+    /// <param name="Name">The name of the product.</param>
+    /// <param name="Thumbnail">The thumbnail path of the product.</param>
+    /// <param name="Tags">The tags of the product.</param>
+    /// <param name="ID">The ID of the product.</param>
+    public record struct DPProductRecordLite(string Name, string? Thumbnail, IReadOnlyList<string> Tags, long ID) { }
 }
