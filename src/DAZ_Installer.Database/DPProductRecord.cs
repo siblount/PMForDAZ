d9c2032eb2a505966550bb5281b2e5f79bf8778c
Solomon Blount <74916907+siblount@users.noreply.github.com>
2024-01-21 20:40:03 -0500

    rename time parameter to "Date"

--- src/DAZ_Installer.Database/DPProductRecord.cs
+++ src/DAZ_Installer.Database/DPProductRecord.cs
@@ -8,16 +8,16 @@ namespace DAZ_Installer.Database
     /// </summary>
     /// <param name="Name">The identified product name.</param>
     /// <param name="Authors">The identified authors of the product.</param>
-    /// <param name="Time">The date/time at which the product was installed.</param>
+    /// <param name="Date">The date/time at which the product was installed.</param>
     /// <param name="ThumbnailPath">The relative (or absolute) path of the thumbnail of the product.</param>
     /// <param name="ArcName">The archive name containing the product.</param>
     /// <param name="Tags">The tags identified for the product.</param>
     /// <param name="Files">The files identified for the product.</param>
     /// <param name="ID">The unique identifier of the product in the database.</param>
-    public record DPProductRecord(string Name, IReadOnlyList<string> Authors, DateTime Time, string? ThumbnailPath, 
+    public record DPProductRecord(string Name, IReadOnlyList<string> Authors, DateTime Date, string? ThumbnailPath, 
         string ArcName, string Destination, IReadOnlyList<string> Tags, IReadOnlyList<string> Files, long ID) {
         public string TagsString => string.Join(", ", Tags);
         public string AuthorsString => string.Join(", ", Authors);
-        public long TimeStamp => Time.ToFileTimeUtc();
+        public long TimeStamp => Date.ToFileTimeUtc();
     }
 }
