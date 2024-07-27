4f11e22e5fad43866a959707dcafc6270adfeebe
Solomon Blount <74916907+siblount@users.noreply.github.com>
2024-07-27 12:16:58 -0400

    Updated get coverage script.

--- src/get_coverage.bat
+++ src/get_coverage.bat
@@ -1,2 +1,13 @@
+@echo off
+
+rem Remove TestResults folders
+if exist .\DAZ_Installer.DatabaseTests\TestResults rmdir /s /q .\DAZ_Installer.DatabaseTests\TestResults 2>nul
+if exist .\DAZ_Installer.CoreTests\TestResults rmdir /s /q .\DAZ_Installer.CoreTests\TestResults 2>nul
+if exist .\DAZ_Installer.IOTests\TestResults rmdir /s /q .\DAZ_Installer.IOTests\TestResults 2>nul
+if exist .\TestResults rmdir /s /q .\TestResults 2>nul
+rem Remove coveragereport directory
+if exist .\coveragereport rmdir /s /q .\coveragereport 2>nul
+
+rem Run tests and generate coverage report
 dotnet test --collect:"XPlat Code Coverage"
 reportgenerator -reports:"**/*.cobertura.xml" -targetdir:"coveragereport" -reporttypes:Html
