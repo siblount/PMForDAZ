a8da207a8b7c1efc69cab7998f098abfd4fe6002
Solomon Blount <74916907+siblount@users.noreply.github.com>
2024-07-27 22:20:03 -0400

    tested DPDSXElement + minor changes

--- src/DAZ_Installer.Core/DPDSXElement.cs
+++ src/DAZ_Installer.Core/DPDSXElement.cs
@@ -1,33 +1,33 @@
 // This code is licensed under the Keep It Free License V1.
 // You may find a full copy of this license at root project directory\LICENSE
 
+using System.Runtime.CompilerServices;
+
 namespace DAZ_Installer.Core
 {
     public class DPDSXElement
     {
         public readonly Dictionary<string, string> attributes = new();
-        public Memory<char> InnerText = new char[] { };
+        public Memory<char> InnerText = Array.Empty<char>();
         public string TagName = string.Empty;
-        public Memory<char> TotalMessage = new char[] { };
+        public Memory<char> TotalMessage = Array.Empty<char>();
         public bool IsSelfClosingElement = false;
         public bool MessageIncludesEnding = false;
         public int BeginningIndex = -1;
         public int EndIndex = -1;
 
-        public DPDSXElement Parent;
-        public DPDSXElement NextSibling;
-        public DPDSXElement PreviousSibling;
+        public DPDSXElement? Parent;
+        public DPDSXElement? NextSibling;
+        public DPDSXElement? PreviousSibling;
         public List<DPDSXElement> Children = new();
         public DPDSXElementCollection File;
         public DPDSXElement() { }
         /// <summary>
         /// DSXElements within the given index range will have its parent set to this DSXElement and added to children array.
         /// </summary>
-        /// <param name="beginningIndex">The total beginning index of the buffer array.</param>
-        /// <param name="endIndex">The total end index of the buffer array.</param>
         public void ParentChildrenWithinIndexRange()
         {
-            DPDSXElement workingSibling = NextSibling;
+            DPDSXElement? workingSibling = NextSibling;
             while (workingSibling != null && IndexInRange(BeginningIndex, EndIndex, workingSibling))
             {
                 workingSibling.Parent = this;
@@ -36,10 +36,10 @@ namespace DAZ_Installer.Core
             }
         }
 
-        protected static bool IndexInRange(int beginningIndex, int endIndex, DPDSXElement element)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool IndexInRange(int beginningIndex, int endIndex, DPDSXElement element)
         {
-            if (element.BeginningIndex > beginningIndex && element.EndIndex < endIndex) return true;
-            else return false;
+            return (element.BeginningIndex > beginningIndex && element.EndIndex < endIndex);
         }
 
     }
---
+++ src/DAZ_Installer.CoreTests/DPDSXElementTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAZ_Installer.Core;
+using System.Collections.Generic;
+
+namespace DAZ_Installer.Core.Tests
+{
+    [TestClass]
+    public class DPDSXElementTests
+    {
+        [TestMethod]
+        public void ParentChildrenWithinIndexRange_NoSiblingsWithinRange_NoChildrenAdded()
+        {
+            // Arrange
+            var parent = new DPDSXElement { BeginningIndex = 0, EndIndex = 10 };
+            var sibling = new DPDSXElement { BeginningIndex = 15, EndIndex = 20 };
+            parent.NextSibling = sibling;
+
+            // Act
+            parent.ParentChildrenWithinIndexRange();
+
+            // Assert
+            Assert.AreEqual(0, parent.Children.Count);
+            Assert.IsNull(sibling.Parent);
+        }
+
+        [TestMethod]
+        public void ParentChildrenWithinIndexRange_AllSiblingsWithinRange_AllChildrenAdded()
+        {
+            // Arrange
+            var parent = new DPDSXElement { BeginningIndex = 0, EndIndex = 30 };
+            var sibling1 = new DPDSXElement { BeginningIndex = 5, EndIndex = 10 };
+            var sibling2 = new DPDSXElement { BeginningIndex = 15, EndIndex = 20 };
+            parent.NextSibling = sibling1;
+            sibling1.NextSibling = sibling2;
+
+            // Act
+            parent.ParentChildrenWithinIndexRange();
+
+            // Assert
+            Assert.AreEqual(2, parent.Children.Count);
+            Assert.AreEqual(parent, sibling1.Parent);
+            Assert.AreEqual(parent, sibling2.Parent);
+            CollectionAssert.Contains(parent.Children, sibling1);
+            CollectionAssert.Contains(parent.Children, sibling2);
+        }
+
+        [TestMethod]
+        public void ParentChildrenWithinIndexRange_SomeSiblingsWithinRange_OnlyWithinRangeChildrenAdded()
+        {
+            // Arrange
+            var parent = new DPDSXElement { BeginningIndex = 0, EndIndex = 25 };
+            var sibling1 = new DPDSXElement { BeginningIndex = 5, EndIndex = 10 };
+            var sibling2 = new DPDSXElement { BeginningIndex = 15, EndIndex = 20 };
+            var sibling3 = new DPDSXElement { BeginningIndex = 30, EndIndex = 35 };
+            parent.NextSibling = sibling1;
+            sibling1.NextSibling = sibling2;
+            sibling2.NextSibling = sibling3;
+
+            // Act
+            parent.ParentChildrenWithinIndexRange();
+
+            // Assert
+            Assert.AreEqual(2, parent.Children.Count);
+            Assert.AreEqual(parent, sibling1.Parent);
+            Assert.AreEqual(parent, sibling2.Parent);
+            Assert.IsNull(sibling3.Parent);
+            CollectionAssert.Contains(parent.Children, sibling1);
+            CollectionAssert.Contains(parent.Children, sibling2);
+            CollectionAssert.DoesNotContain(parent.Children, sibling3);
+        }
+
+        [TestMethod]
+        public void ParentChildrenWithinIndexRange_SiblingOnBoundary_NotAdded()
+        {
+            // Arrange
+            var parent = new DPDSXElement { BeginningIndex = 0, EndIndex = 20 };
+            var sibling1 = new DPDSXElement { BeginningIndex = 5, EndIndex = 10 };
+            var sibling2 = new DPDSXElement { BeginningIndex = 20, EndIndex = 25 };
+            parent.NextSibling = sibling1;
+            sibling1.NextSibling = sibling2;
+
+            // Act
+            parent.ParentChildrenWithinIndexRange();
+
+            // Assert
+            Assert.AreEqual(1, parent.Children.Count);
+            Assert.AreEqual(parent, sibling1.Parent);
+            Assert.IsNull(sibling2.Parent);
+            CollectionAssert.Contains(parent.Children, sibling1);
+            CollectionAssert.DoesNotContain(parent.Children, sibling2);
+        }
+    }
+}
