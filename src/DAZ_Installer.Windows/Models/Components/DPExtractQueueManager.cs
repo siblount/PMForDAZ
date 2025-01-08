4e32825db72672479c872f7caab362b704c172b7
Solomon Blount <74916907+siblount@users.noreply.github.com>
2025-01-08 00:11:09 -0500

    create extract queue manager

---
+++ src/DAZ_Installer.Windows/Models/Components/DPExtractQueueManager.cs
@@ -0,0 +1,29 @@
+﻿using DAZ_Installer.Windows.DP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAZ_Installer.Windows.Models.Components
+{
+    internal class DPExtractQueueManager : IDPExtractQueueManager
+    {
+        public static readonly DPExtractQueueManager Instance = new();
+
+        public void AddArchiveToQueue(string filePath, string groupKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateArchiveStatus(string filePath, DPArchiveStatus status, IEnumerable<DPArchiveInfo.ErrorInfo> errors)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
